6c426c86850a22f5872d133979f5a25b13964fe4
fflashhz <fatehdifateh@gmail.com>
2026-09-19 00:51:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,4 @@
-Skip to content
-fatehdifateh
-bot
-Repository navigation
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security and quality
-Insights
-Settings
-Files
-Go to file
-t
-T
-.github/workflows
-bot.yml
-index.js
-package.json
-bot
-/
-index.js
-in
-main
-
-Edit
-
-Preview
-Indent mode
-
-Spaces
-Indent size
-
-4
-Line wrap mode
-
-No wrap
-Editing index.js file contents
-  1
-  2
-  3
-  4
-  5
-  6
-  7
-  8
-  9
- 10
- 11
- 12
- 13
- 14
- 15
- 16
- 17
- 18
- 19
- 20
- 21
- 22
- 23
- 24
- 25
- 26
- 27
- 28
- 29
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, PermissionFlagsBits } = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -77,11 +8,16 @@ const client = new Client({
 });
 
 client.on('ready', () => {
-    console.log(`${client.user.tag} aktif!`);
+    console.log(`${client.user.tag} aktif ve yetki odaklı çalışıyor!`);
 });
 
 client.on('messageCreate', async message => {
-    if (message.author.bot) return; 
+    if (message.author.bot || !message.guild) return; 
+
+    const botYetkileri = message.channel.permissionsFor(message.guild.members.me);
+    if (!botYetkileri || !botYetkileri.has(PermissionFlagsBits.ManageMessages)) {
+        return; 
+    }
 
     try {
         await message.channel.send({ 
@@ -90,11 +26,8 @@ client.on('messageCreate', async message => {
         });
         await message.delete();
     } catch (error) {
-        console.error("Hata:", error);
+        console.error("Hata oluştu:", error);
     }
 });
 
 client.login(process.env.TOKEN);
-
-Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
-While the code is focused, press Alt+F1 for a menu of operations.
