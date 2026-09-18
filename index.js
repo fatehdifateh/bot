021d0aefede7081a84becb88285deace840e6274
fflashhz <fatehdifateh@gmail.com>
2026-09-19 00:43:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,72 @@
+Skip to content
+fatehdifateh
+bot
+Repository navigation
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security and quality
+Insights
+Settings
+Files
+Go to file
+t
+T
+.github/workflows
+bot.yml
+index.js
+package.json
+bot
+/
+index.js
+in
+main
+
+Edit
+
+Preview
+Indent mode
+
+Spaces
+Indent size
+
+4
+Line wrap mode
+
+No wrap
+Editing index.js file contents
+  1
+  2
+  3
+  4
+  5
+  6
+  7
+  8
+  9
+ 10
+ 11
+ 12
+ 13
+ 14
+ 15
+ 16
+ 17
+ 18
+ 19
+ 20
+ 21
+ 22
+ 23
+ 24
+ 25
+ 26
+ 27
+ 28
+ 29
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
     intents: [
@@ -26,3 +95,6 @@ client.on('messageCreate', async message => {
 });
 
 client.login(process.env.TOKEN);
+
+Use Control + Shift + m to toggle the tab key moving focus. Alternatively, use esc then tab to move to the next interactive element on the page.
+While the code is focused, press Alt+F1 for a menu of operations.
